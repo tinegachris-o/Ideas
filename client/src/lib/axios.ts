296f3f9ea1026394ae5d02e985177319e765538b
tinegachris-o <tinegachris797@gmail.com>
2025-11-24 16:22:31 +0300

    error fixing

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,48 +1,49 @@
 import axios from "axios";
-import { getStoredAccessToken,setStoredAccessToken} from "./authToken";
+import { getStoredAccessToken, setStoredAccessToken } from "./authToken";
 import { refreshAccessToken } from "@/api/auth";
+
+//const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"; // fallback
+const BASE_URL = import.meta.env.VITE_API_URL || "https://ideas-z5cj.onrender.com";
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
-// Attach token on refresh
+
+// Attach token to requests
 api.interceptors.request.use((config) => {
   const token = getStoredAccessToken();
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
-// Refresh token after expire
+// Refresh token on 401
 api.interceptors.response.use(
   (res) => res,
   async (error) => {
     const originalRequest = error.config;
-
     if (
       error.response?.status === 401 &&
       !originalRequest._retry &&
-      !originalRequest.url.includes('/auth/refresh')
+      !originalRequest.url.includes("/auth/refresh")
     ) {
       originalRequest._retry = true;
-
       try {
         const { accessToken: newToken } = await refreshAccessToken();
         setStoredAccessToken(newToken);
         originalRequest.headers.Authorization = `Bearer ${newToken}`;
         return api(originalRequest);
       } catch (err) {
-        console.error('Refresh token failed', err);
+        console.error("Refresh token failed", err);
       }
     }
-
     return Promise.reject(error);
   }
 );
+
 export default api;
