5d3359598723a48d522dd966bd7e34e6f7c9736d
tinegachris-o <tinegachris797@gmail.com>
2025-11-24 14:44:23 +0300

    profuction mode

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -1,4 +1,5 @@
-import api from "@/lib/axios";
+import api from '@/lib/axios';
+
 export const registerUser = async ({
   name,
   email,
@@ -9,43 +10,47 @@ export const registerUser = async ({
   password: string;
 }) => {
   try {
-    const res = await api.post("/auth/register", {
+    const res = await api.post('/auth/register', {
       name,
       email,
       password,
     });
+
     return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || "Failed to register";
+  } catch (err: any) {
+    const message = err.response?.data?.message || 'Failed to register';
     throw new Error(message);
   }
 };
-export const loginUser=async(credentials:{
-email:string;
-password:string;
-})=>{
+
+export const loginUser = async (credentials: {
+  email: string;
+  password: string;
+}) => {
   try {
-    const res=await api.post('/auth/login',credentials)
-    return res.data
-  } catch (error:any) {
-    const message = error.response?.data?.message || "Failed to login";
+    const res = await api.post('/auth/login', credentials);
+    return res.data;
+  } catch (err: any) {
+    const message = err.response?.data?.message || 'Failed to login';
     throw new Error(message);
   }
-}
-export const logoutUser=async()=>{
+};
+
+export const logoutUser = async () => {
   try {
-    await api.post("/auth/logout")
-  } catch (error:any) {
-     const message = error.response?.data?.message || "Failed to logout user";
-     throw new Error(message);
+    await api.post('/auth/logout');
+  } catch (err: any) {
+    const message = err.response?.data?.message || 'Failed to logout';
+    throw new Error(message);
   }
-}
+};
+
 export const refreshAccessToken = async () => {
   try {
-    const res = await api.post("/auth/refresh");
+    const res = await api.post('/auth/refresh');
     return res.data;
-  } catch (error: any) {
-    const message = error.response?.data?.message || "Failed to refreshToken";
+  } catch (err: any) {
+    const message = err.response?.data?.message || 'Failed to refresh token';
     throw new Error(message);
   }
 };
