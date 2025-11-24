58b61c7989e43dd1614e18ce65f6571184520eb7
tinegachris-o <tinegachris797@gmail.com>
2025-11-24 12:09:34 +0300

    client changed

--- server/app.js
+++ server/app.js
@@ -18,13 +18,23 @@ connectDB();
 
 // Global middleware
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://ideas-git-main-tinegas-projects.vercel.app"
+];
 
-app.use(
-  cors({
-    origin: "http://localhost:5174",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // for Postman or curl
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
